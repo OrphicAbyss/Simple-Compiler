ffdac69308fff623e22b551093c12944ccf637e3
Chris Hallson <drlabman@gluonporridge.net>
2012-04-25 10:57:39 +1000

    Working interpreter.

--- interpreter.pas
+++ interpreter.pas
@@ -2,10 +2,15 @@
 program Cradle;
 {--------------------------------------------------------------}
 { Constant Declarations }
-const TAB = ^I;
+const
+   TAB = ^I;
+   CR = #13;
+   LF = #10;
 {--------------------------------------------------------------}
 { Variable Declarations }
-var Look: char;              { Lookahead Character }
+var 
+   Look: char;              { Lookahead Character }
+   Table: Array['A'..'Z'] of integer;
 {--------------------------------------------------------------}
 { Read New Character From Input Stream }
 procedure GetChar;
@@ -74,6 +79,16 @@ begin
    GetNum := Value;
 end;
 {--------------------------------------------------------------}
+{ Recognize and Skip Over a Newline }
+procedure NewLine;
+begin
+   if (Look = CR) or (Look = LF) then begin
+      GetChar;
+      if Look = LF then
+         GetChar;
+   end;
+end;
+{--------------------------------------------------------------}
 { Output a String with Tab }
 procedure Emit(s: string);
 begin
@@ -87,6 +102,21 @@ begin
    WriteLn;
 end;
 {---------------------------------------------------------------}
+{ Parse and Translate a Math Factor }
+function Expression: integer; Forward;
+function Factor: integer;
+begin
+   if Look = '(' then begin
+      Match('(');
+      Factor := Expression;
+      Match(')');
+      end
+   else if IsAlpha(Look) then
+      Factor := Table[GetName]
+   else
+       Factor := GetNum;
+end;
+{---------------------------------------------------------------}
 function IsMulop(c: char): boolean;
 begin
    IsMulop := c in ['*', '/'];
@@ -96,16 +126,16 @@ end;
 function Term: integer;
 var Value: integer;
 begin
-   Value := GetNum;
+   Value := Factor;
    while IsMulop(Look) do begin
       case Look of
        '*': begin
                Match('*');
-               Value := Value * GetNum;
+               Value := Value * Factor;
             end;
        '/': begin
                Match('/');
-               Value := Value div GetNum;
+               Value := Value div Factor;
             end;
       end;
    end;
@@ -140,15 +170,54 @@ begin
    Expression := Value;
 end;
 {--------------------------------------------------------------}
+{ Parse and Translate an Assignment Statement }
+procedure Assignment;
+var Name: char;
+begin
+   Name := GetName;
+   Match('=');
+   Table[Name] := Expression;
+end;
+{--------------------------------------------------------------}
+{ Input Routine }
+procedure Input;
+begin
+   Match('?');
+   Read(Table[GetName]);
+end;
+{--------------------------------------------------------------}
+{ Output Routine }
+procedure Output;
+begin
+   Match('!');
+   WriteLn(Table[GetName]);
+end;
+{---------------------------------------------------------------}
+{ Initialize the Variable Area }
+procedure InitTable;
+var i: char;
+begin
+   for i := 'A' to 'Z' do
+      Table[i] := 0;
+end;
+{--------------------------------------------------------------}
 { Initialize }
 procedure Init;
 begin
    GetChar;
-   Writeln(Expression);
+   repeat
+      case Look of
+       '?': Input;
+       '!': Output;
+       else Assignment;
+      end;
+      NewLine;
+   until Look = '.';
 end;
 {--------------------------------------------------------------}
 { Main Program }
 begin
+   InitTable;
    Init;
 end.
 {--------------------------------------------------------------}
